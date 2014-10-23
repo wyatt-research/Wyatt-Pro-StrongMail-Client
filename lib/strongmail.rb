25af717e11b59ef8211685a7b9b033181ee265a6
Charles Sprayberry <charles.sprayberry@wyattresearch.com>
2014-10-23 14:51:27 -0400

    adds rest of api commands to Strongmail module

--- lib/strongmail.rb
+++ lib/strongmail.rb
@@ -14,8 +14,28 @@ module Strongmail
     yield(configuration)
   end
 
-  def self.fetch_or_create_member(email)
-    api.fetch_or_create_member email
+  def self.fetch_member(email)
+    api.fetch_member email
+  end
+
+  def self.create_member(member_attr)
+    api.create_member member_attr
+  end
+
+  def self.update_member(member)
+    api.update_member member
+  end
+
+  def self.fetch_subscriptions(member, lists = nil)
+    api.fetch_subscriptions member, lists
+  end
+
+  def self.subscribe(member, lists)
+    api.subscribe member, lists
+  end
+
+  def self.unsubscribe(member, lists)
+    api.unsubscribe member, lists
   end
 
   def self.api
